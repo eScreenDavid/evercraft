454b2f5ef8b35d5871b481da4a8c25aaae84c6ee
brentmitchell <brent.mitchell25@gmail.com>
2018-07-11 10:35:07 -0500

    Write impl

--- Evercraft/Character.py
+++ Evercraft/Character.py
@@ -2,9 +2,18 @@
 class Character:
   def __init__(self):
         self.name = None
+        self.alignment = None
 
   def getName(self):
         return self.name
 
   def setName(self, value):
       self.name = value
+
+  def getAlignment(self):
+      return self.alignment
+
+  def setAlignment(self, value):
+      if value not in ['Neutral', 'Good', 'Evil']:
+            raise ValueError('Alignment not found.')
+      self.alignment = value
--- Evercraft/test_character.py
+++ Evercraft/test_character.py
@@ -12,17 +12,22 @@ class CharacterTest(unittest.TestCase):
     def test_alignment_good(self):
         player = Character()
         player.setAlignment("Good")
-        self.assertEqual(player.getAlignment, "Good")
+        self.assertEqual(player.getAlignment(), "Good")
 
     def test_alignment_evil(self):
         player = Character()
         player.setAlignment("Evil")
-        self.assertEqual(player.getAlignment, "Evil")
+        self.assertEqual(player.getAlignment(), "Evil")
 
     def test_alignment_neutral(self):
         player = Character()
         player.setAlignment("Neutral")
-        self.assertEqual(player.getAlignment, "Neutral")
+        self.assertEqual(player.getAlignment(), "Neutral")
+    
+    def test_alignment_exception(self):
+        player = Character()
+        with self.assertRaises(ValueError):
+            player.setAlignment('')
 
 if __name__ == '__main__':
     unittest.main()
