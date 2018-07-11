61861fe6f53ed30ed111bb1cc49b5f687af32773
David Timberlake <david.timberlake@escreen.local>
2018-07-11 10:25:08 -0500

    alignment testing added

--- Evercraft/test_character.py
+++ Evercraft/test_character.py
@@ -1,13 +1,28 @@
 
 import unittest
-from character import Character
+from Character import Character
 
 class CharacterTest(unittest.TestCase):
 
-    def test_upper(self):
+    def test_playername(self):
       player = Character()
       player.setName("MasterDonkey")
       self.assertEqual(player.getName(),  "MasterDonkey")
+
+    def test_alignment_good(self):
+        player = Character()
+        player.setAlignment("Good")
+        self.assertEqual(player.getAlignment, "Good")
+
+    def test_alignment_evil(self):
+        player = Character()
+        player.setAlignment("Evil")
+        self.assertEqual(player.getAlignment, "Evil")
+
+    def test_alignment_neutral(self):
+        player = Character()
+        player.setAlignment("Neutral")
+        self.assertEqual(player.getAlignment, "Neutral")
 
 if __name__ == '__main__':
     unittest.main()
