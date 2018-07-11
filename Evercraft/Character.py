72a95c6e5d306f8fe7cff6e13ddbccca44bd264a
David Timberlake <david.timberlake@escreen.local>
2018-07-11 10:45:37 -0500

    Added HP and Armor to character

--- Evercraft/Character.py
+++ Evercraft/Character.py
@@ -1,5 +1,9 @@
 
 class Character:
+
+  armor = 10 #defaults to 10
+  hitpoints = 5 #defaults to 5
+
   def __init__(self):
         self.name = None
         self.alignment = None
@@ -17,3 +21,9 @@ class Character:
       if value not in ['Neutral', 'Good', 'Evil']:
             raise ValueError('Alignment not found.')
       self.alignment = value
+
+  def getArmor(self):
+      return self.armor
+
+  def getHitPoints(self):
+      return self.hitpoints
